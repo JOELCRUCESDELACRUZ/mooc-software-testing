da78f269d9e4ea1d4b6bfdeb8beba2819e198b0d
JOELCRUCES <joel.cruces@ucsm.edu.pe>
2025-03-28 22:32:45 -0500

    test: Se prueban 4 casos distintos para asegurar la funcionalidad del cifrado en CaesarShiftCipherTest

--- src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
+++ src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
@@ -1,5 +1,27 @@
 package tudelft.caesarshift;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CaesarShiftCipherTest {
+    @Test
+    void PruebaCifrado1() {
+        CaesarShiftCipher csc = new CaesarShiftCipher();
+        assertEquals("jqnc", csc.CaesarShiftCipher("hola", 2));
+    }
+    @Test
+    void PruebaCifrado2() {
+        CaesarShiftCipher csc = new CaesarShiftCipher();
+        assertEquals("cem", csc.CaesarShiftCipher("zbj", 3));
+    }
+    @Test
+    void PruebaCifrado3() {
+        CaesarShiftCipher csc = new CaesarShiftCipher();
+        assertEquals("jq nc", csc.CaesarShiftCipher("ho la", 2));
+    }
+    @Test
+    void PruebaCifrado4() {
+        CaesarShiftCipher csc = new CaesarShiftCipher();
+        assertEquals("rnt", csc.CaesarShiftCipher("mio", 5));
+    }
 
 }
