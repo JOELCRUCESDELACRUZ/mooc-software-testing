a180f09d19ab79162566d3d325b6300173c6c7d7
JOELCRUCES <joel.cruces@ucsm.edu.pe>
2025-03-28 22:07:53 -0500

    fix: Se cambio la condicional del codio GHappy para validar como verdadero 2 g juntas

--- src/main/java/tudelft/ghappy/GHappy.java
+++ src/main/java/tudelft/ghappy/GHappy.java
@@ -1,18 +1,19 @@
 package tudelft.ghappy;
 
+
 public class GHappy {
 
     public boolean gHappy(String str) {
-        assert str!=null;
-        for(int i = 0; i < str.length(); i++) {
-            if(str.charAt(i) == 'g') {
-                if (i >= 0 && str.charAt(i-1) == 'g') { continue; }
-                if (i+1 < str.length() && str.charAt(i+1) == 'g') { continue; }
+        if (str == null) return false;
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == 'g') {
+                if ((i > 0 && str.charAt(i - 1) == 'g') || (i + 1 < str.length() && str.charAt(i + 1) == 'g')) {
+                    continue;
+                }
                 return false;
             }
         }
-
         return true;
-
     }
 }
