7992e06b8c106ec88c0d9ad825769d76c3579370
JOELCRUCES <joel.cruces@ucsm.edu.pe>
2025-03-28 22:09:23 -0500

    test: Se prueban 6 casos donde la g  validara si es true o false

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,44 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class GHappyTest {
+
+    @org.junit.Test
+    @Test
+    public void testAllHappyGs() {
+        GHappy gHappy = new GHappy();
+        assertTrue(gHappy.gHappy("xxggxx"));
+    }
+
+    @Test
+    public void testSingleUnhappyG() {
+        GHappy gHappy = new GHappy();
+        assertFalse(gHappy.gHappy("xxgxx"));
+    }
+
+    @Test
+    public void testUnhappyGAtEnd() {
+        GHappy gHappy = new GHappy();
+        assertFalse(gHappy.gHappy("xxggyygxx"));
+    }
+
+    @Test
+    public void testNoGs() {
+        GHappy gHappy = new GHappy();
+        assertTrue(gHappy.gHappy("abcdef"));
+    }
+
+    @Test
+    public void testOnlyHappyGs() {
+        GHappy gHappy = new GHappy();
+        assertTrue(gHappy.gHappy("gggg"));
+    }
+
+    @Test
+    public void testSingleG() {
+        GHappy gHappy = new GHappy();
+        assertFalse(gHappy.gHappy("g"));
+    }
 }
